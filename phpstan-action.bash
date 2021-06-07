e865fb7d98c6a7138fa553a45a47d9da272637cd
Greg Bowler <greg.bowler@g105b.com>
2021-06-07 17:44:00 +0100

    expose host environment variables to docker runner

--- phpstan-action.bash
+++ phpstan-action.bash
@@ -60,9 +60,29 @@ then
 	command_string+=($ACTION_ARGS)
 fi
 
+dockerKeys=()
+while IFS= read -r line
+do
+	dockerKeys+=( $(echo "$line" | cut -f1 -d=) )
+done <<<$(docker run --rm "${docker_tag}" env)
+
+while IFS= read -r line
+do
+	key=$(echo "$line" | cut -f1 -d=)
+	if printf '%s\n' "${dockerKeys[@]}" | grep -q -P "^${key}\$"
+	then
+    		echo "Skipping env variable $key" >> output.log
+	else
+		echo "$line" >> DOCKER_ENV
+	fi
+done <<<$(env)
+
 echo "Command: " "${command_string[@]}" >> output.log 2>&1
+echo "::set-output name=full_command::${command_string}"
+
 docker run --rm \
 	--volume "${github_action_path}/phpstan.phar":/usr/local/bin/phpstan \
 	--volume "${GITHUB_WORKSPACE}":/app \
 	--workdir /app \
+	--env-file ./DOCKER_ENV \
 	${docker_tag} "${command_string[@]}"
